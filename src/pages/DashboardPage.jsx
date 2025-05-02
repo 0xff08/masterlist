bdd72034d29c211391d0fccb1f180b7295602e1d
Bryan Glen Suello <11388006+bgsuello@users.noreply.github.com>
2025-05-02 21:43:16 +0800

    updated dashboard

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,18 +1,55 @@
 // pages/DashboardPage.js
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import {ConfigProvider, Layout, Row, Statistic} from "antd";
 import bgVideo from '../assets/Tapelect-Bg.mp4'
+import supabase from "../supabase.js";
 
 function DashboardPage() {
 
-  const [balance, setBalance] = useState(1.5e10);
+  const [count, setCount] = useState(0);
 
-  const handleLogout = async () => {
-    try {
-    } catch (err) {
-      console.error(err);
+  const [seedAmount, setSeedAmount] = useState(0);
+
+  const [multiplier, setMultiplier] = useState(0);
+
+  useEffect(() => {
+
+    const getSeedAmount = async () => {
+      const {data, error} = await supabase
+        .from('tblX9A2B7GK')
+        .select('*')
+        .limit(1)
+      console.log(data)
+      setSeedAmount(data[0].seed)
+      setMultiplier(data[0].multiplier)
     }
-  };
+
+    if (!seedAmount || !multiplier) {
+      getSeedAmount()
+    }
+  }, []);
+
+  useEffect(() => {
+    const fetchCount = async () => {
+      const {data, error} = await supabase
+        .rpc('count_status1')
+      console.log('count', data)
+      if (data) setCount(data);
+    };
+
+    fetchCount(); // Initial count
+
+    const subscription = supabase
+      .channel("dataflux827_changes")
+      .on("postgres_changes", {event: "*", schema: "public", table: "dataflux827"}, () => {
+        fetchCount(); // Fetch new count on change
+      })
+      .subscribe();
+
+    return () => {
+      supabase.removeChannel(subscription); // Cleanup subscription
+    };
+  }, []);
 
   return (
     <div style={{width: '100vw', height: '100vh', margin: 0, padding: 0}}>
@@ -26,7 +63,12 @@ function DashboardPage() {
         </video>
         <div className="content-overlay">
           <Row style={{height: '100vh'}} align={'middle'} justify={'center'}>
-            <Statistic value={balance} precision={2} valueStyle={{fontFamily: '"inter", sans-serif', fontWeight: 700, fontSize: '64pt', color: 'white'}}/>
+            <Statistic value={seedAmount - (count * multiplier)} precision={2} valueStyle={{
+              fontFamily: '"inter", sans-serif',
+              fontWeight: 700,
+              fontSize: '64pt',
+              color: 'white'
+            }}/>
           </Row>
         </div>
       </div>
