adc999558f5573f8f5b4703fbd1a10a7c439dcd0
Bryan Glen Suello <11388006+bgsuello@users.noreply.github.com>
2025-05-05 18:55:39 +0800

    updated LeaderBoard.jsx

--- src/pages/LeaderBoard.jsx
+++ src/pages/LeaderBoard.jsx
@@ -1,18 +1,16 @@
 // pages/DashboardPage.js
 import React, {useEffect, useState} from "react";
-import {ConfigProvider, Layout, Progress, Row, Statistic, Table} from "antd";
+import {Card, Col, ConfigProvider, Flex, Layout, Progress, Row, Statistic, Table, Typography} from "antd";
 import bgVideo from '../assets/Tapelect-Bg.mp4'
 import supabase from "../supabase.js";
 
 function LeaderBoard() {
 
   const [count, setCount] = useState(0);
-
   const [seedAmount, setSeedAmount] = useState(0);
-
   const [multiplier, setMultiplier] = useState(0);
-
   const [leaders, setLeaders] = useState(null);
+  const [overall, setOverall] = useState(false);
 
   useEffect(() => {
 
@@ -41,12 +39,22 @@ function LeaderBoard() {
       if (data) setLeaders(data);
     };
 
+    const fetchOverall = async () => {
+      const {data, error} = await supabase
+        .from('vubue8fiesa3_status_by_fp')
+        .select('*')
+        .limit(100)
+      setOverall(data[0])
+    }
+
+    fetchOverall()
     fetchCount(); // Initial count
 
     const subscription = supabase
       .channel("vubue8fiesa3_changes")
       .on("postgres_changes", {event: "*", schema: "public", table: "vuBue8Fiesa3"}, () => {
         fetchCount(); // Fetch new count on change
+        fetchOverall()
       })
       .subscribe();
 
@@ -55,9 +63,54 @@ function LeaderBoard() {
     };
   }, []);
 
+  const twoColors = {
+    '0%': '#108ee9',
+    '100%': '#87d068',
+  };
+
+  const conicColors = {
+    '0%': '#87d068',
+    '50%': '#ffe58f',
+    '100%': '#ffccc7',
+  };
+
   return (
     <Layout style={{width: '100vw', height: '100vh', margin: 0, padding: 0}}>
+      <Row gutter={10} style={{margin: '20px 5px 10px 5px' }}>
+        <Col span={12}>
+
+          <Card variant="borderless">
+            <Flex justify="space-between" align={'center'}>
+              <Statistic
+                title="Completed Focal Person"
+                value={overall.completed_fp}
+                precision={0}
+                valueStyle={{color: '#3f8600'}}
+                suffix={<> / {overall.total_fp}</>}
+              />
+              <Progress size={100} percent={parseInt((overall.completed_fp / overall.total_fp) * 100)}
+                        type='dashboard' strokeColor='#3f8600'/>
+            </Flex>
+          </Card>
+        </Col>
+        <Col span={12}>
+          <Card variant="borderless">
+            <Flex justify="space-between" align={'center'}>
+              <Statistic
+                title="Completed Liners"
+                value={overall.completed_liners}
+                precision={0}
+                valueStyle={{color: '#3f8600'}}
+                suffix={<> / {overall.total_liners}</>}
+              />
+              <Progress size={100} percent={parseInt((overall.completed_liners / overall.total_liners) * 100)}
+                        type='dashboard' strokeColor='#3f8600'/>
+            </Flex>
+          </Card>
+        </Col>
+      </Row>
       <Table
+        style={{margin: 10}}
         rowKey='fp'
         dataSource={leaders}
         size="small"
@@ -65,12 +118,12 @@ function LeaderBoard() {
           {
             title: 'Focal Person',
             dataIndex: 'fp',
-            render: (d) => (<span style={{textTransform: 'capitalize'}}>{d}</span> )
+            render: (d) => (<span style={{textTransform: 'capitalize'}}>{d}</span>)
           }, {
             title: 'Barangay',
             dataIndex: 'barangay',
             align: 'left',
-            render: (d) => (<span style={{textTransform: 'capitalize'}}>{d}</span> )
+            render: (d) => (<span style={{textTransform: 'capitalize'}}>{d}</span>)
           },
           {
             title: 'Members Total',
@@ -88,7 +141,7 @@ function LeaderBoard() {
           },
           {
             title: 'Progress',
-            render: (_, record)=>{
+            render: (_, record) => {
               return (
                 <ConfigProvider
                   theme={{
@@ -99,8 +152,8 @@ function LeaderBoard() {
                   }}
                 >
                   <Progress
-                    percent={parseInt((record.status_1/record.total)*100)}
-                    percentPosition={{ align: 'center', type: 'inner' }}
+                    percent={parseInt((record.status_1 / record.total) * 100)}
+                    percentPosition={{align: 'center', type: 'inner'}}
                     size={[200, 15]}
                     strokeColor="#24AC58"
                     trailColor="lightgray"
